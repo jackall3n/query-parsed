c336950b733cb61ea80c2141499a9965bad5b5ef
Jack Allen <jackallensemail+git@gmail.com>
2022-05-16 10:08:05 +0100

    1.0.0

---
+++ README.md
@@ -0,0 +1,68 @@
+# query-parsed 🔎
+
+<p>
+<a href="https://www.npmjs.com/query-parsed"><img src="https://img.shields.io/npm/v/query-parsed.svg" alt="NPM Version" /></a>
+<a href="https://www.npmjs.com/query-parsed"><img src="https://img.shields.io/npm/l/query-parsed.svg" alt="Package License" /></a>
+<a href="https://www.npmjs.com/query-parsed"><img src="https://img.shields.io/npm/dm/query-parsed.svg" alt="NPM Downloads" /></a>
+</p>
+
+## Blurb
+
+A hook for Next.js which can parse and return a query-string object, even on load.
+
+## Installation
+
+```shell
+npm install query-parsed
+```
+
+```shell
+yarn add query-parsed
+```
+
+## Usage
+
+### Parsed
+
+```typescript
+import queryparse from 'query-parsed';
+
+// Return Type = { test: number }
+const parsed = queryparse.parse("test=1", { test: Number });
+```
+
+### Arrays
+
+```typescript
+import queryparse from 'query-parsed';
+
+// Return Type = { test: number[] }
+const parsed = queryparse.parse("test=1,2", { test: [Number] }, { arrayFormat: "comma" });
+```
+
+### Typed
+
+```typescript
+import queryparse from 'query-parsed';
+
+// Return Type = { test: string }
+const parsed = queryparse.parse<{ test: string }>("test=1");
+```
+
+### Default
+
+```typescript
+import queryparse from 'query-parsed';
+
+// Return Type = Record<string, string | string[]>
+const parsed = queryparse.parse("test=1");
+```
+
+
+## API
+
+### Supported Parse Types
+
+```typescript
+String | Boolean | Number
+```
---
+++ index.ts
@@ -0,0 +1,115 @@
+import querystring, {
+  ParseOptions as QueryParseOptions,
+  StringifyOptions as QueryStringifyOptions
+} from 'query-string';
+
+type BaseTypes = typeof Boolean | typeof Number | typeof String
+type Types = BaseTypes | BaseTypes[]
+
+type IsString<T> = T extends typeof String ? true : T extends string ? true : false;
+type IsBoolean<T> = T extends typeof Boolean ? true : false;
+type IsNumber<T> = T extends typeof Number ? true : false;
+
+type ParseType<T> =
+  IsString<T> extends true ? string :
+    IsBoolean<T> extends true ? boolean :
+      IsNumber<T> extends true ? number : (string | string[])
+
+type Parsed<T> = {
+  [K in keyof T]: ParseType<T[K]>
+}
+
+export type Config = Record<string, Types>;
+export type ParseOptions = QueryParseOptions;
+export type StringifyOptions = QueryStringifyOptions;
+
+export function parse<C extends Config>(query: string, config?: C, options?: ParseOptions): Parsed<C> {
+  const parsed = querystring.parse(query, options) as Parsed<C>;
+
+  const entries = Object.entries(config ?? {})
+
+  if (!entries.length) {
+    return parsed;
+  }
+
+  return entries.reduce((query, [key, type]) => {
+    const value = parsed[key] as string | string[];
+
+    const result = (() => {
+      if (!Array.isArray(type)) {
+        return type(value)
+      }
+
+      if (Array.isArray(value) && Array.isArray(type)) {
+        return value.map(v => type[0](v))
+      }
+
+      return value
+    })()
+
+    return ({
+      ...query,
+      [key]: result
+    })
+  }, {} as Parsed<C>)
+}
+
+export function stringify(object: Record<string, any>, options?: StringifyOptions) {
+  return querystring.stringify(object, options);
+
+}
+
+export function serializeConfig(types: Record<string, Types>) {
+  const serialized = Object.entries(types).reduce((serialized, [key, type]) => {
+    const typeConstructor = Array.isArray(type) ? type[0] : type;
+
+    const typeString = (() => {
+      switch (typeConstructor) {
+        case Number:
+          return 'Number';
+        case String:
+          return 'String';
+        case Boolean:
+          return 'Boolean';
+      }
+
+      return 'Unknown'
+    })();
+
+    return {
+      ...serialized,
+      [key]: Array.isArray(type) ? `[]${typeString}` : typeString
+    }
+  }, {});
+
+  return JSON.stringify(serialized);
+}
+
+export function deserializeConfig(serialized: string): Record<string, Types> {
+  const json = JSON.parse(serialized) as Record<string, string>;
+
+  return Object.entries(json).reduce((deserialized, [key, type]) => {
+    const isArray = type.startsWith('[]');
+    const typeString = isArray ? type.substring(2) : type;
+
+    const typeConstructor = (() => {
+      switch (typeString) {
+        case 'Number':
+          return Number;
+        case 'String':
+          return String;
+        case 'Boolean':
+          return Boolean;
+      }
+
+      return undefined
+    })();
+
+    return {
+      ...deserialized,
+      [key]: isArray ? [typeConstructor] : typeConstructor
+    }
+  }, {});
+}
+
+export default { parse, stringify }
